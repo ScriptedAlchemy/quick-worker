3b4854b732ff3fd6af70837ea9e08941b03a6156
ScriptedAlchemy <zackary.l.jackson@gmail.com>
2023-11-29 23:33:39 -0800

    stashing delets

--- examples/website/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import App from './App'
-
-test('renders something', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/QuickJS/)
-  expect(linkElement).toBeInTheDocument()
-})
